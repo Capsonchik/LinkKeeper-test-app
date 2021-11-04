e10a7cf520e5ccb7066b951cc402a6eb67df9f22
Capsonchik <Serega00790@mail.ru>
2021-11-04 19:36:32 +0300

    new screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+
 import MainComponent from "./components/MainComponent/MainComponent";
-import {BrowserRouter, Route, Routes, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import LoginScreen from "./components/LoginScreen/LoginScreen";
+import AddLink from "./components/AddLink/AddLink";
 
 
 function App() {
     return (
         <BrowserRouter>
             <div className='container'>
-                <Routes>
-                    <Route exact path='/' element={<MainComponent />}/>
-                </Routes>
+                <Switch>
+                    <Route exact path='/'>
+                        <LoginScreen/>
+                    </Route>
+                    <Route exact path='/main'>
+                        <MainComponent/>
+                    </Route>
+                </Switch>
             </div>
         </BrowserRouter>
     );
---
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router-dom";
+
+const LoginScreen = () => {
+    return(
+        <Link to = '/main'> На главную</Link>
+    )
+}
+
+export default LoginScreen;
