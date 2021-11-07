4da839e00fe8d61a6ed3191342a191f05051fa24
Capsonchik <80291699+Capsonchik@users.noreply.github.com>
2021-11-07 17:53:12 +0300

    Revert "home page lite"

--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -1,31 +1,8 @@
-import s from './LoginScreen.module.css'
-import {Swiper} from 'swiper'
-import SwiperSlide from 'react'
-
+import {Link} from "react-router-dom";
 
 const LoginScreen = () => {
     return(
-        <div className={s.main}>
-            <h1 className={s.title}>Lin Keeper</h1>
-            {/*<div className={s.block}></div>*/}
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                ...
-            </Swiper>
-            <p className={s.text}>(Текст про то, что это за приложение)</p>
-            <button className={s.btnLogin}>Вход с помощью Google</button>
-            <button className={s.btnLogin}>Вход с помощью Facebook</button>
-            <button className={s.btnLogin}>Вход с помощью VK</button>
-            <button className={s.btnLogin}>Вход с помощью Yandex</button>
-        </div>
+        <Link to = '/main'> На главную</Link>
     )
 }
 
